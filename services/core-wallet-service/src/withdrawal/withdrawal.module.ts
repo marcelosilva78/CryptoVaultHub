452c7fe4099cf5458e38143f8ee88cd314a3ded2
Marcelo Silva <marcelomortificy@gmail.com>
2026-04-23 12:12:07 -0400

    fix(co-sign): import CoSignModule in WithdrawalModule for DI resolution

--- services/core-wallet-service/src/withdrawal/withdrawal.module.ts
+++ services/core-wallet-service/src/withdrawal/withdrawal.module.ts
@@ -1,12 +1,13 @@
-import { Module } from '@nestjs/common';
+import { Module, forwardRef } from '@nestjs/common';
 import { WithdrawalService } from './withdrawal.service';
 import { WithdrawalExecutorService } from './withdrawal-executor.service';
 import { WithdrawalController } from './withdrawal.controller';
 import { BlockchainModule } from '../blockchain/blockchain.module';
 import { ComplianceModule } from '../compliance/compliance.module';
+import { CoSignModule } from '../co-sign/co-sign.module';
 
 @Module({
-  imports: [BlockchainModule, ComplianceModule],
+  imports: [BlockchainModule, ComplianceModule, forwardRef(() => CoSignModule)],
   controllers: [WithdrawalController],
   providers: [WithdrawalService, WithdrawalExecutorService],
   exports: [WithdrawalService, WithdrawalExecutorService],
